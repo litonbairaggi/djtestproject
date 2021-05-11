5f0a570491980f92b919222bfa8ebeacd0e352b9
litonbairaggi <litonovi2013@gmail.com>
2021-05-11 23:17:15 +0600

    image restore

--- tuition/urls.py
+++ tuition/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import contact
+from .views import contact, postview
 
 urlpatterns = [
     #path('contact/', views.contact, name='contact'),
     path('contact/',contact,name="contact"),
+    path('posts/',postview,name="posts"),
 ]
--- tuition/views.py
+++ tuition/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Contact
+from .models import Contact, Post
 from .froms import ContactForm
 # Create your views here.
     
@@ -11,3 +11,10 @@ def contact(request):
     else:
         form = ContactForm()    
     return render(request, 'contact.html', {'form': form})
+
+def postview(request):
+    post = Post.objects.all()
+    context ={
+        'post': post
+    }
+    return render(request, 'tuition/postview.html', context)     
