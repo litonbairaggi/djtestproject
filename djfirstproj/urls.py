857d90dd888f79901f5ea41978c4aaf4c9ded66d
litonbairaggi <litonovi2013@gmail.com>
2021-05-13 16:30:07 +0600

    TemplateView add to firstproj views

--- djfirstproj/urls.py
+++ djfirstproj/urls.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 from django.urls import path, include
-from .views import home
+from .views import home, HomeView
+from django.views.generic import TemplateView
 
 from django.conf import settings
 from django.conf.urls.static import static
@@ -8,5 +9,7 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/',home, name="home"),
+    path('',HomeView.as_view(), name="homeview"),
+    #path('',TemplateView.as_view(template_name='home.html'), name="homeview"),
     path('tuition/', include('tuition.urls')),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- djfirstproj/views.py
+++ djfirstproj/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, HttpResponse
-
+from django.views.generic import TemplateView
 
 def home(request):
     if request.method=="POST":        
@@ -10,4 +10,13 @@ def home(request):
         'name':name,
     }
     return render(request, 'home.html', context)
+
+class HomeView(TemplateView):
+    template_name = 'home.html'    
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context["message"] = "Welcome to our website" 
+        context["message2"] = "Welcome to our website again" 
+        return context
+    
 
