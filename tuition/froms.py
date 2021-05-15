06784104bb3689d46598d9b7bf33c2a318b65e95
litonbairaggi <litonovi2013@gmail.com>
2021-05-15 19:54:31 +0600

    form design from fomrs.py

--- tuition/froms.py
+++ tuition/froms.py
@@ -3,9 +3,23 @@ from .models import Contact, Post
 
 
 class ContactForm(forms.ModelForm):
+    # It's use for only one 1 object/ field
+    # name=forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your name'}), label='Your name')
     class Meta:
         model=Contact 
         fields ='__all__'
+        widgets={
+            'name':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your name'}),
+            'phone':forms.TextInput(attrs={'class':'form-control', 'placeholder':'Enter your phone'}),
+            'content':forms.Textarea(attrs={'class':'form-control', 'placeholder':'Say somthing', 'rows':8})
+        }
+        labels={
+            'name':'Your name',
+            'phone':'Your phone',
+            'content':'Your words'
+        }
+
+
 class ContactFormtwo(forms.ModelForm):
     class Meta:
         model=Contact 
